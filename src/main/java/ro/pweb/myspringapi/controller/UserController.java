1cf360ac76d94794d03e5dc5d6740e41076dfefd
Marina Simionescu <marina.simionescu21@gmail.com>
2023-04-19 11:27:13 +0300

    user done

--- src/main/java/ro/pweb/myspringapi/controller/UserController.java
+++ src/main/java/ro/pweb/myspringapi/controller/UserController.java
@@ -1,37 +1,47 @@
 package ro.pweb.myspringapi.controller;
 
+import jakarta.annotation.security.RolesAllowed;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ro.pweb.myspringapi.dto.UserDTO;
 import ro.pweb.myspringapi.exceptions.UserNotFoundException;
 import ro.pweb.myspringapi.service.UserService;
 import ro.pweb.myspringapi.entity.User;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "api/v1/users")
 public class UserController {
 
     private UserService userService;
+    @Autowired
+    private ModelMapper modelMapper;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<User>> getUsers() {
-        List<User> users = userService.getUsers();
-        return ResponseEntity.ok().body(users);
+    public ResponseEntity<List<UserDTO>> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers()
+                .stream().map(user -> modelMapper.map(user, UserDTO.class))
+                .collect(Collectors.toList()));
     }
 
     @GetMapping(path = "{id}")
-    public Optional<User> getUserById(@PathVariable Long id) {
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
         Optional<User> user = userService.getById(id);
-        return ResponseEntity.ok().body(user).getBody();
+        UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+        return ResponseEntity.ok().body(userDTO);
     }
 
+    @RolesAllowed("Admin")
     @PostMapping("/create")
     public HttpStatus createUser(@RequestBody User user) throws Exception {
         try {
@@ -42,12 +52,14 @@ public class UserController {
         }
     }
 
+    @RolesAllowed("Admin")
     @PutMapping(path = "{id}")
     public User updateUser(@PathVariable Long id, @RequestBody User user) {
         User newUser = userService.updateUser(id, user);
         return ResponseEntity.ok().body(newUser).getBody();
     }
 
+    @RolesAllowed("Admin")
     @DeleteMapping(path = "{id}")
     public HttpStatus deleteUser(@PathVariable Long id) {
         try {
